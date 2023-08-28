6564bf675ea048d0d688fc60c2aecfe67746cd69
David Danilchik <davdd@mac.com>
2023-08-28 16:48:43 -0700

    got best book to best book the book

--- src/Components/BestBooks.jsx
+++ src/Components/BestBooks.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import axios from 'axios';
 
+
 const PORT = import.meta.env.VITE_server_url;
 
 
@@ -15,7 +16,7 @@ class BestBooks extends React.Component {
 
   fetchAllBooks = () => {
     console.log('reaching to server')
-    axios.get(PORT)
+    axios.get(`${PORT}/books`)
       .then(response => {
         this.setState({ books: response.data });
         console.log(response.data)
@@ -39,16 +40,22 @@ class BestBooks extends React.Component {
 
         {this.state.books.length ? (
               <Carousel>
-                    {this.state.books.map(<Carousel.Item> 
+                    {this.state.books.map((books, idx) =>
+                    <Carousel.Item key={idx}> 
+                            <img
+                              className="d-block w-100"
+                              src="https://media.discordapp.net/attachments/1136025197523259402/1145868153285525514/arkuris42069_a_bookshelf_drawn_in_a_realistic_depiction_bcf85a72-12e1-4da1-bc7c-0aa73e44f912.png?width=889&height=889"
+                              alt="First slide"
+                            />
                       <Carousel.Caption>
                         <h3>
-                          {this.state.title}
+                          {books.title}
                         </h3>
                         <p>
-                          {this.state.descriptions}
+                          {books.description}
                         </p>
                         <p>
-                          Availible? {this.state.status}
+                          Availible? {books.status}
                         </p>
                       </Carousel.Caption>
                     </Carousel.Item>)
