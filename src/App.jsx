7854ddf5131121775d718c4a80ed3c318c9f5405
David Danilchik <davdd@mac.com>
2023-08-28 17:07:26 -0700

    added aboot us, made text white so you can see it lol

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import BestBooks from './Components/BestBooks';
+import About from './About'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -22,7 +23,11 @@ class App extends React.Component {
               element={<BestBooks />}
             >
             </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route 
+              exact path="/About"
+              element={<About />}
+            >
+            </Route>
           </Routes>
           <Footer />
         </Router>
