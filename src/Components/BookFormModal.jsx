90e7bb49737d120ba44de5e6ab0d6bb8795be6bf
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-30 11:01:50 -0700

    updated css form styling

--- src/Components/BookFormModal.jsx
+++ src/Components/BookFormModal.jsx
@@ -12,12 +12,11 @@ class BookFormModal extends React.Component {
     super(props);
   }
 
-
   handleCreate = async (book) => {
     console.log(book);
     let response = await axios.post(`${SERVER_URL}/books`, book);
-    this.props.addNewBook(response.data)
-  }
+    this.props.addNewBook(response.data);
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
@@ -27,15 +26,17 @@ class BookFormModal extends React.Component {
       description: description.value,
       status: status.value,
     });
-  }
+  };
 
-handleDelete = async (id) => {
-  await axios.delete(`${PORT}/books/${id}`);
-  this.setState({ books: this.state.books.filter(books => {
-    console.log(books._id);
-    return books._id !== id
-  })});
-}
+  handleDelete = async (id) => {
+    await axios.delete(`${PORT}/books/${id}`);
+    this.setState({
+      books: this.state.books.filter((books) => {
+        console.log(books._id);
+        return books._id !== id;
+      }),
+    });
+  };
 
   render() {
     return (
@@ -48,12 +49,51 @@ handleDelete = async (id) => {
             Add a New Book
           </Modal.Header>
           <Modal.Body style={{ width: '500px' }}>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="title" />
-              <input type="text" name="description" />
-              <input type="radio" name="status" value="true" />
-              <input type="radio" name="status" value="false" />
-              <button type="submit">Add</button>
+            <form
+              onSubmit={this.handleSubmit}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <label style={{ marginBottom: '10px' }}>
+                Title:
+                <input
+                  type="text"
+                  name="title"
+                  style={{ marginTop: '5px', padding: '5px' }}
+                />
+              </label>
+              <label style={{ marginBottom: '10px' }}>
+                Description:
+                <input
+                  type="text"
+                  name="description"
+                  style={{ marginTop: '5px', padding: '5px' }}
+                />
+              </label>
+              <div style={{ display: 'flex', marginBottom: '10px' }}>
+                <label style={{ marginRight: '10px' }}>
+                  <input type="radio" name="status" value="true" /> True
+                </label>
+                <label>
+                  <input type="radio" name="status" value="false" /> False
+                </label>
+              </div>
+              <button
+                type="submit"
+                style={{
+                  padding: '8px 15px',
+                  backgroundColor: '#007bff',
+                  color: '#fff',
+                  border: 'none',
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                }}
+              >
+                Add
+              </button>
             </form>
           </Modal.Body>
           <Modal.Footer>
